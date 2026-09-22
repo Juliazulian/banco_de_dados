c353c83c8a4277c04961ae7258a661b3540cd08d
Gamahen011 <ricaohgp@gmail.com>
2026-09-22 17:12:58 -0300

    SELECT

--- SELECT/01.sql
+++ SELECT/01.sql
@@ -1,1 +1,11 @@
---
+--Listar todas as informações da tabela de animais
+SELECT * FROM `animais`;
+
+--Listar apenas o nome, email e cidade de todos os tutores
+SELECT `nome`, `email`, `cidade` FROM `tutores`;
+
+--Listar o nome e a especialidade de todos os veterinários
+SELECT `nome`, `especialidade` FROM `veterinários`;
+
+--Listar apenas o motivo e o custo de todas as consultas
+SELECT `motivo`, `custo` FROM `consultas`;
